68db7cf7a2ebc50a87a36da283878fb7f265e385
U-FISHBOWLDOMAIN\lbeatty <lbeatty@lbeattynote.FISHBOWLDOMAIN.com>
2016-04-08 21:11:36 -0500

    code clean-up

--- js/viewModels/documents.js
+++ js/viewModels/documents.js
@@ -1,73 +1,17 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'ojs/ojbutton', 'ojs/ojdialog'],
-        function (oj, ko, $)
-        {
-            function dialogModel() {
+     function(oj, ko, $) {
+          var dialogModel = function() {
 
-                var self = this;
-                self.handleOpen = $("#buttonOpener").click(function () {
+
+               this.handleOpen = $("#buttonOpener").click(function() {
                     alert("made it!");
                     $("#modalDialog1").ojDialog("open");
-                });
+               });
 
-                self.handleOKClose = $("#okButton").click(function () {
+               this.handleOKClose = $("#okButton").click(function() {
                     $("#modalDialog1").ojDialog("close");
-                });
-            }
-            return dialogModel();
+               });
+          };
+          return dialogModel();
 
-        });
-//$(document).ready(function () {
-//
-//    //Oracle DoCS REST API information
-//    var consts = {
-//        //url will be personal to your own domain
-//        url: "https://dcs-fishbowlsolutions.documents.us2.oraclecloud.com",
-//        apiPath: "/documents/api",
-//        apiVersion: "/1.1",
-//        files: "/files",
-//        data: "/D252B819CF83B3B3C2F512ED580C088D60E36785B51D/data"
-//    };
-//
-//    //this brings back all of the folders
-//    var requestPath = consts.url + consts.apiPath + consts.apiVersion + consts.files + consts.data;
-//
-//    var getFiles = function () {
-//        $.ajax({
-//            type: 'GET',
-//            url: requestPath,
-//            dataType: 'text',
-//            crossDomain: true,
-//            //has to be false to work with CORS plug-in
-//            xhrFields: {
-//                withCredentials: false
-//            },
-//            beforeSend: function (xhr) {
-//                xhr.setRequestHeader('Authorization',
-//                        'Basic ' + btoa(''));
-//            },
-//            success: function (data) {
-//                console.log(data);
-//
-//
-//                //TODO loop through the data here data.items[i].name gives name of folder
-//            },
-//            error: function (jqXHR, textStatus, errorThrown) {}
-//        });
-//    };
-//
-//    getFiles();
-//
-//});
-//
-//
-//
+     });
--- js/viewModels/folders.js
+++ js/viewModels/folders.js
@@ -1,99 +1,105 @@
 define(['ojs/ojcore', 'knockout', 'jquery', 'ojs/ojknockout', 'promise', 'ojs/ojlistview', 'ojs/ojarraytabledatasource', 'ojs/ojbutton', 'ojs/ojinputtext'],
-        function (oj, ko, $)
-        {
-            var BetterListModel = function () {
-                this.allItems = ko.observableArray([{"id": "", "name": ""}]);
+     function(oj, ko, $) {
+          var BetterListModel = function() {
 
-                this.selectedItems = ko.observableArray([]);
-
-                this.dataSource = new oj.ArrayTableDataSource(this.allItems, {idAttribute: "id"});
-
-                var folders;
-
-                var newFolders;
-
-                var consts = {
+               this.allItems = ko.observableArray([{
+                    "id": "",
+                    "name": ""
+               }]);
+               this.selectedItems = ko.observableArray([]);
+               this.dataSource = new oj.ArrayTableDataSource(this.allItems, {
+                    idAttribute: "id"
+               });
+               var folders;
+               var newFolders;
+               var consts = {
                     //url will be personal to your own domain
                     url: "https://dcs-fishbowlsolutions.documents.us2.oraclecloud.com",
                     apiPath: "/documents/api",
                     apiVersion: "/1.1",
                     mainFolder: "/folders/self",
-                    mainFolderItems: "/folders/items",
-                    subFolderItems: "/folders/{folder.GUID}/items"
-                };
+                    mainFolderItems: "/folders/items"
+               };
+               //this brings back all of the folders
+               var requestPath = consts.url + consts.apiPath + consts.apiVersion + consts.mainFolderItems;
+               var folderInfo = consts.url + consts.apiPath + consts.apiVersion;
 
-                //this brings back all of the folders                   
-                var requestPath = consts.url + consts.apiPath + consts.apiVersion + consts.mainFolderItems;
 
-                var folderInfo = consts.url + consts.apiPath + consts.apiVersion;
+               //login variables
+               this.username = ko.observable();
+               this.password = ko.observable();
+               var credentials;
 
-                var getFolders = function () {
+               //clicking on the submit button gets the form values, and submits ajax request to Oracle Docs
+               this.login = function(data, event) {
+
+                    username = $("#username").val();
+                    password = $("#password").val();
+                    credentials = username + ":" + password;
+
                     $.ajax({
-                        type: 'GET',
-                        url: requestPath,
-                        dataType: 'text',
-                        crossDomain: true,
-                        //has to be false to work with CORS plug-in
-                        xhrFields: {
-                            withCredentials: false
-                        },
-                        beforeSend: function (xhr) {
-                            xhr.setRequestHeader('Authorization',
-                                    //TODO put your username and password the parens like this: btoa('jsim@fishbowlsolutions.com:passwordgoeshere')
-                                    'Basic ' + btoa(''));
-                        },
-                        success: function (data) {
-                            folders = JSON.parse(data);
-                            console.log(folders);
+                         type: 'GET',
+                         url: requestPath,
+                         dataType: 'text',
+                         crossDomain: true,
+                         //has to be false to work with CORS plug-in
+                         xhrFields: {
+                              withCredentials: false
+                         },
+                         beforeSend: function(xhr) {
+                              xhr.setRequestHeader('Authorization',
+                                   'Basic ' + btoa(credentials));
+                         },
+                         success: function(data) {
+                              folders = JSON.parse(data);
+                              console.log(folders);
+                              for (var i = 0; i < folders.items.length; i++) {
+                                   console.log(folders.items[i]);
+                                   if (folders.items[i].type === "folder") {
+                                        allItems.push({
+                                             id: folders.items[i].id,
+                                             name: folders.items[i].name
 
-                            for (var i = 0; i < folders.items.length; i++) {
-                                console.log(folders.items[i]);
-                                if (folders.items[i].type === "folder") {
-                                    allItems.push({
-                                        id: folders.items[i].id,
-                                        name: folders.items[i].name
+                                        });
+                                   }
+                                   console.log(allItems());
+                              }
 
-                                    });
-
-                                }
-                                console.log(allItems());
-
-                            }
-
-                        },
-                        error: function (jqXHR, textStatus, errorThrown) {}
+                         },
+                         error: function(jqXHR, textStatus, errorThrown) {}
                     });
 
-                };
-                getFolders();
+                    $(".wrapper").hide();
+                    $("#listViewContainer").show();
+                    return true;
+               };
 
-                var clickFolder = function () {
+               var clickFolder = function() {
                     var clickedFolderID = allItems()[1].id;
                     console.log("The clicked folder is: " + clickedFolderID);
                     $.ajax({
-                        type: 'GET',
-                        url: folderInfo + "/folders/" + clickedFolderID + "/items",
-                        dataType: 'text',
-                        crossDomain: true,
-                        xhrFields: {
-                            withCredentials: false
-                        },
-                        beforeSend: function (xhr) {
-                            xhr.setRequestHeader('Authorization', 'Basic'
-                                    + btoa(''));
-                        },
-                        success: function (data) {
+                         type: 'GET',
+                         url: folderInfo + "/folders/" + clickedFolderID + "/items",
+                         dataType: 'text',
+                         crossDomain: true,
+                         xhrFields: {
+                              withCredentials: false
+                         },
+                         beforeSend: function(xhr) {
+                              xhr.setRequestHeader('Authorization', 'Basic' + btoa(''));
+                         },
+                         success: function(data) {
 
-                            var subFolders = JSON.parse(data);
-                            console.log("The sub-folders are: " + subFolders);
-                        }
+                              var subFolders = JSON.parse(data);
+                              console.log("The sub-folders are: " + subFolders);
+                         }
 
                     });
-                };
-
-                $(document).on("click", "#ui-id-16", function () {
+               };
+               $(document).on("click", "#ui-id-18", function() {
+                    alert("made it!");
                     clickFolder();
-                });
-            };
-            return BetterListModel();
-        });
+               });
+          };
+          return BetterListModel();
+     });
